b94da04d46acf2e252a5c6e4b35c9ddd7309b1a7
sarsur99 <sorenisme@gmail.com>
2023-10-25 15:07:09 -0700

    header

---
+++ src/Header.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Header.css'; // Create a corresponding CSS file
+
+function Header() {
+  return (
+    <header className="header">
+      <div className="header-logo">
+        <span className="header-logo-text">Soren Sonstein</span>
+      </div>
+      <nav className="header-navigation">
+        <ul className="header-nav-list">
+          <li className="header-nav-item"><a href="#projects">Projects</a></li>
+          <li className="header-nav-item"><a href="#about">About Me</a></li>
+          <li className="header-nav-item"><a href="#resume">Resume</a></li>
+          <li className="header-nav-item"><a href="#contact">Contact</a></li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
